6a779d4eae5b938522866e26347dafa76bf3f1fb
huy <huy.duong@web.de>
2023-03-06 10:41:58 +0100

    change: zsh to bash

--- fsd_environment.sh
+++ fsd_environment.sh
@@ -1,17 +1,17 @@
 #!/bin/zsh
 
-CATKIN_SHELL=zsh
+CATKIN_SHELL=bash
 
-export FSD_ROOT=$( cd "$(dirname "${(%):-%N}" )" && pwd)
+export FSD_ROOT=$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
 
-printf "Sourcing fsd_skeleton/devel/setup.zsh\n"
+printf "Sourcing fsd_skeleton/devel/setup.bash\n"
 
 # check whether devel folder exists
 if [ -f "${FSD_ROOT}/devel/setup.bash" ]; then
     # source setup.sh from same directory as this file
-    source "${FSD_ROOT}/devel/setup.zsh"
+    source "${FSD_ROOT}/devel/setup.bash"
 else
-    source "/opt/ros/noetic/setup.zsh"
+    source "/opt/ros/noetic/setup.bash"
     printf "You need to build first before you can source\n"
     printf "Run 'catkin build' in the skeleton_repo directory\n"
 fi
